854b1e4ffbc9d34fcee6b8649141a3baf4600959
raulromerap <raul.romera@iesnervion.com>
2024-02-07 09:41:10 +0100

    07/02/2024

--- EjerciciosString/src/ejerciciosstring/Ejercicio02.java
+++ EjerciciosString/src/ejerciciosstring/Ejercicio02.java
@@ -2,30 +2,58 @@ package ejerciciosstring;
 
 import java.util.Scanner;
 
+/**
+ * Esta clase pertenece al ejercicio 2 del tema 4.2 "String"
+ */
 public class Ejercicio02 {
 
+	/**
+	 * Esta funcion se encarga de crear una contraseña, se le muestra a los
+	 * jugadores un mensaje para que adivinen la contraseña, si lo que escribe el
+	 * jugador 1 no es la contraseña se pasa al jugador 2 el turno, si cualquiera de
+	 * los dos acierta se corta el bucle y muestra quien a sido el ganador
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+
+		// Se llama a la función Scanner para crear un objeto de esta
 		Scanner lectura = new Scanner(System.in);
-		
+
+		// Se crea una cadena que contiene la contraseña
 		String contraseña = "odiobd001";
-		
-		char[] letrasContraseña = contraseña.toCharArray();
-		
+
 		String jugador1 = "";
-		
+
 		String jugador2 = "";
-		
-		char[] letrasJugador1;
-		
-		char[] letrasJugador2
-		
+
 		do {
-		System.out.println("Adivina la contraseña");
-		jugador1 = lectura.nextLine();
-		
-		}while()
-		
+
+			System.out.println("Jugador 1 - Adivina la contraseña");
+			jugador1 = lectura.nextLine();
+
+			if (!jugador1.equals(contraseña)) {
+				System.out.println(jugador1.compareTo(contraseña));
+			} else if (jugador1.equals(contraseña)) {
+				break;
+			}
+
+			System.out.println("Jugador 2 - Adivina la contraseña");
+			jugador2 = lectura.nextLine();
+
+			if (!jugador2.equals(contraseña)) {
+				System.out.println(jugador2.compareTo(contraseña));
+			} else if (jugador1.equals(contraseña)) {
+				break;
+			}
+
+		} while (!jugador1.equals(contraseña) || !jugador2.equals(contraseña));
+
+		if (jugador1.equals(contraseña)) {
+			System.out.println("El jugador 1 ha ganado");
+		} else {
+			System.out.println("El jugador 2 ha ganado");
+		}
 	}
 
 }
