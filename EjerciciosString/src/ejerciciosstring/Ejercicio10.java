1186cb8a7c76391f34e29e9832fd70e44ab0069e
raulromerap <raulromerap@DESKTOP-BKVLOPR.home>
2024-02-15 17:42:27 +0100

    15/02/2024

--- EjerciciosString/src/ejerciciosstring/Ejercicio10.java
+++ EjerciciosString/src/ejerciciosstring/Ejercicio10.java
@@ -4,28 +4,59 @@ import java.util.Scanner;
 
 public class Ejercicio10 {
 
-	private static final char[] CONJUNTO1 =
+	private static final char[] CONJUNTO1 = 
 		{ 'e', 'i', 'k', 'm', 'p', 'q', 'r', 's', 't', 'u', 'v' };
 
-	private static final char[] CONJUNTO2 =
+	private static final char[] CONJUNTO2 = 
 		{ 'p', 'v', 'i', 'u', 'm', 't', 'e', 'r', 'k', 'q', 's' };
 
 	public static void main(String[] args) {
 
 		Scanner lectura = new Scanner(System.in);
-		
+
+		String frase = "";
+
+		String aux = "";
+
+		String fraseCodificada = "";
+
 		char c;
-		
-		System.out.println("Introduce una letra");
-		c = lectura.next().charAt(0);
-		
+
+		int i = 0;
+
+		System.out.println("Introduce una palabra");
+		frase = lectura.next();
+
+		aux = frase.toLowerCase();
+
+		while (i != frase.length()) {
+
+			c = aux.charAt(i);
+
+			fraseCodificada += codifica(CONJUNTO1, CONJUNTO2, c);
+
+			i++;
+		}
+
+		System.out.println(fraseCodificada);
+
+		lectura.close();
 	}
 
 	public static char codifica(char[] CONJUNTO1, char[] CONJUNTO2, char c) {
 
-		char caracterCodificado = c;
+		char cCodificado = ' ';
 
-		return caracterCodificado;
+		for(int i = 0; i < CONJUNTO1.length; i++) {
+			if(c == CONJUNTO1[i]) {
+				cCodificado = CONJUNTO2[i];
+				break;
+			}else {
+				cCodificado = c;
+			}
+		}
+		
+		return cCodificado;
 
 	}
 }
