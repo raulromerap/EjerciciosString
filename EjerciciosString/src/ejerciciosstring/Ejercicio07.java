332b7a4b612bf0971e0856b772f3dba97c94ae18
raulromerap <raul.romera@iesnervion.com>
2024-02-15 12:04:47 +0100

    15/02/2024

--- EjerciciosString/src/ejerciciosstring/Ejercicio07.java
+++ EjerciciosString/src/ejerciciosstring/Ejercicio07.java
@@ -1,23 +1,45 @@
 package ejerciciosstring;
 
+import java.util.Scanner;
+
 public class Ejercicio07 {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		Scanner lectura = new Scanner(System.in);
+
+		String frase = "";
+
+		String palabra = "";
+
+		int resultado;
+
+		System.out.println("Introduce una frase");
+		frase = lectura.nextLine();
+
+		System.out.println("Introduce una palabra");
+		palabra = lectura.next();
+
+		resultado = cuentaPalabras(frase, palabra);
+
+		System.out.println("La palabra \"" + palabra + "\" se encuentra " + resultado + " veces");
+
+		lectura.close();
+
 	}
 
-	
 	public static int cuentaPalabras(String frase, String palabra) {
-		
+
 		int cont = 0;
-		
-		if(frase.contains(palabra)) {
-			
+
+		String[] palabras = frase.split(" ");
+
+		for (String valor : palabras) {
+			if (valor.equals(palabra))
+				cont++;
 		}
-		
+
 		return cont;
 	}
-	
+
 }
